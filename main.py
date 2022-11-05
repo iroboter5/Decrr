cd5dbcc349d871ed5d281cba0dea45401f4769fc
ghost0210 <103672354+ghost0210@users.noreply.github.com>
2022-11-05 14:07:32 -0400

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ converter = []
 
 API_ID = 19961504
 API_HASH = '28de3a8f4b68b388bfe47bf84d1b124b'
-Token = '5604529921:AAHS2u0P4V4hsyXpb1Scbaxv20Yeaws7e3k' #token del bot
+Token = '5441625614:AAFWN8WC0D-IT_N289gTK9nsm_WNQAmp_48' #token del bot
 
 bot = Client('session', api_id=API_ID, api_hash=API_HASH, bot_token=Token)
 
